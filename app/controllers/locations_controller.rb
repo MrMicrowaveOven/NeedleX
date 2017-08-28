6157f3dbb625af519050b7c8d880d0168ec9506f
Benjamin <benj@zagorski.com>
2017-08-27 21:21:56 -0700

    Database now updates when Save Data is pressed

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,21 +5,38 @@ class LocationsController < ApplicationController
   end
 
   def edit
-    
+
+  end
+
+  def update
+    # Delete all current locations
+    Location.all.each do |location|
+      location.destroy
+    end
+    locations = params[:locations]
+
+    num_locations = params[:locations].keys.length
+    num_locations.times do |location_index|
+      location_object = locations[location_index.to_s]
+      location = {
+        name: location_object[:name],
+        address: location_object[:address],
+        description: location_object[:description],
+        link: location_object[:link],
+        phone_number: location_object[:phone_number],
+      }
+      new_location = Location.new(location)
+      new_location.save!
+    end
+    render json: {yo: "success"}
   end
 
   def create
-    # render json: location_params
-
     @location = Location.new(location_params)
-
     if @location.save
       render json: @location
-
-      # return
     else
       render json: {failure: "location_save_error"}
-      # return
     end
   end
 
