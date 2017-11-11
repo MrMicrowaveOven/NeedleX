bb7bf43028fca384ce7ee02d5d5b6c773ddc05cd
Benjamin <benj@zagorski.com>
2017-11-11 11:27:45 -0800

    Removed exposure of env variable

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def show
-    render json: {GoogleAPIKey: ENV["GOOGLE_API_KEY"]}
+    # render json: {GoogleAPIKey: ENV["GOOGLE_API_KEY"]}
   end
 end
