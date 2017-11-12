ccc6ff73bebba88bce1ffa06f9e82e9e34ea14ba
Benjamin <benj@zagorski.com>
2017-11-11 18:13:07 -0800

    Posting to locations now calls the GDrive sheet and updates the database accordingly

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -59,38 +59,90 @@ class LocationsController < ApplicationController
   end
 
   def create
-    @location = Location.new(location_params)
-    if @location.save
-      [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday].each_with_index do |day, day_index|
-        if params[:location][day] && params[:location][day] != "undefined" && !params[:location][day].empty?
-          # p "Yo, day is vallllllllllllllid!!!"
-          # p params[:location]
-          # p day
-          # p params[:location][day]
-          hours = params[:location][day]
-          # opening_string = hours.match(/\d{1,2}(:(\d{1,2}))([ap]m)/)
-          opening_time = hours.slice(0, hours.index(","))
-          closing_time = hours.slice(hours.index(","), hours.length)
-          # TODO: Error handling for hours input
-          # closing_string = closing_hour.match(/\d{1,2}(:(\d{1,2}))([ap]m)/)
-          opening = Time.parse(opening_time)
-          closing = Time.parse(closing_time)
-          new_availability = Availability.new(
-            {
-              location_id: @location[:id],
-              day_of_week: day_index,
-              opening: opening,
-              closing: closing
-            })
-          new_availability.save!
+    ws = SheetsHelper.get_sheet_data
+
+
+    # important_one = ws[3,1]
+    rows = []
+
+    (2..ws.num_rows).each do |row|
+      rows << []
+      (1..ws.num_cols).each do |col|
+        rows.last << ws[row, col]
+      end
+    end
+
+    rows.each_with_index do |row, index|
+      break if row[1].blank?
+      location = Location.new({
+        name: row[1],
+        address: "#{row[2]}, #{row[3]}, #{row[4]}, #{row[5]}",
+        description: row[6],
+        link: row[7],
+        phone_number: row[8],
+      })
+      unless location.save
+        render json: {error: "failure to save Location ##{index}"}
+        return
+      end
+
+      (9..22).each do |col|
+        next if col % 2 == 0
+        next if row[col].blank?
+        opening_time = row[col].to_i
+        closing_time = row[col + 1].to_i
+        day_of_week = (col - 9) / 2
+        availability = Availability.new({
+          location_id: location.id,
+          opening: Time.new(2000, 1, 1, opening_time / 100, opening_time % 100),
+          closing: Time.new(2000, 1, 1, closing_time / 100, closing_time % 100),
+          day_of_week: day_of_week
+        })
+        unless availability.save
+          render json: {error: "failure to save availability #{day_of_week} for Location ##{index}"}
+          return
         end
       end
-      render json: @location
-    else
-      # p "==============================="
-      # p "failure to save location"
-      render json: {failure: "location_save_error"}
+      # row[9..22].each_with_index do |opening_time, day_of_week|
+      #   next if opening_time % 2 == 0
+      #   closing_time = row[]
+      # end
+
     end
+
+    render json: {location: "Success!"}
+    # @location = Location.new(location_params)
+    # if @location.save
+    #   [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday].each_with_index do |day, day_index|
+    #     if params[:location][day] && params[:location][day] != "undefined" && !params[:location][day].empty?
+    #       # p "Yo, day is vallllllllllllllid!!!"
+    #       # p params[:location]
+    #       # p day
+    #       # p params[:location][day]
+    #       hours = params[:location][day]
+    #       # opening_string = hours.match(/\d{1,2}(:(\d{1,2}))([ap]m)/)
+    #       opening_time = hours.slice(0, hours.index(","))
+    #       closing_time = hours.slice(hours.index(","), hours.length)
+    #       # TODO: Error handling for hours input
+    #       # closing_string = closing_hour.match(/\d{1,2}(:(\d{1,2}))([ap]m)/)
+    #       opening = Time.parse(opening_time)
+    #       closing = Time.parse(closing_time)
+    #       new_availability = Availability.new(
+    #         {
+    #           location_id: @location[:id],
+    #           day_of_week: day_index,
+    #           opening: opening,
+    #           closing: closing
+    #         })
+    #       new_availability.save!
+    #     end
+    #   end
+    #   render json: @location
+    # else
+    #   # p "==============================="
+    #   # p "failure to save location"
+    #   render json: {failure: "location_save_error"}
+    # end
   end
 
   private
