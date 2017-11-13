c1a6a8d5d3ce75666c37389413f2fa0356ff8548
Benjamin <benj@zagorski.com>
2017-11-12 21:53:16 -0800

    OAuth isn't working on the server

--- app/helpers/sheets_helper.rb
+++ app/helpers/sheets_helper.rb
@@ -8,7 +8,7 @@ module SheetsHelper
     # first time and save it to config.json file for later usages.
     # See this document to learn how to create config.json:
     # https://github.com/gimite/google-drive-ruby/blob/master/doc/authorization.md
-    session = GoogleDrive::Session.from_config("config.json")
+    session = GoogleDrive::Session.login_with_oauth(ENV['refresh_token'])
 
     # First worksheet of
     # https://docs.google.com/spreadsheet/ccc?key=pz7XtlQC-PYx-jrVMJErTcg
