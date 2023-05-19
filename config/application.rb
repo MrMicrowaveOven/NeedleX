828748d4ad589c5a7d9182c92fa7cc37e3d30294
mrmicrowaveoven <benj@zagorski.com>
2023-05-19 16:06:34 -0500

    Updated config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module NeedleX
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = "Pacific Time (US & Canada)"
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
